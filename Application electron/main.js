5bdd188a72209f1a97e39b4f654caf5e9a4a7103
Sedrick <sedrickidd@hotmail.com>
2018-09-25 16:07:12 -0400

    Ajout d'un header sur la page html d'electron

--- Application electron/main.js
+++ Application electron/main.js
@@ -6,7 +6,7 @@ const { app, BrowserWindow } = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ width: 1366, height: 768 })
   
     // and load the index.html of the app.
     win.loadFile('index.html')
