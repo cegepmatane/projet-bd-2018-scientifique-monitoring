eba20edc5ace3e9373dce26120c266ce6799dd23
Sedrick <sedrickidd@hotmail.com>
2018-10-02 16:44:35 -0400

    Modification du script de connection pour recevoir les donnees #10

--- Application electron/html_data/connectionNode.js
+++ Application electron/html_data/connectionNode.js
@@ -6,14 +6,28 @@
    {
        connection = io.connect('http://vps202433.vps.ovh.ca:8080');
        connection.on('salutation', saluer);
+       connection.on('donneeBouee', recevoirDonneeBouee);
    }
 
    function saluer(salution)
    {
-       var resultat = document.getElementById("resultatNode");
-       resultat.innerHTML = JSON.parse(salution); 
+       //var resultat = document.getElementById("resultatNode");
+       //resultat.innerHTML = JSON.parse(salution); 
    }
+
+   this.requeteDonneeBouee = function()
+   {
+    connection.emit('envoyer-donnee');
+   };
+
+   function recevoirDonneeBouee(donnee)
+   {
+    console.log("les donnee serait recu" + donnee);
+   }
+
+
    init();
+   this.requeteDonneeBouee();
 
 }
 )();
