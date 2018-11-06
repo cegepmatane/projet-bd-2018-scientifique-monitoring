7032b55c187257f223edc6b6f833025e40879cf1
Sedrick <sedrickidd@hotmail.com>
2018-11-06 16:25:24 -0500

    ajout de la simulation des position des boues #10

--- Application electron/html_data/connectionNode.js
+++ Application electron/html_data/connectionNode.js
@@ -1,12 +1,14 @@
-(function ()
+this.ConnectionNode = function ()
 {
    var connection;
+   var positionBouee;
 
    function init()
    {
        connection = io.connect('http://vps202433.vps.ovh.ca:8080');
        connection.on('salutation', saluer);
        connection.on('donneeBouee', recevoirDonneeBouee);
+       connection.on('positionBouee', recevoirPositionBouee);
    }
 
    function saluer(salution)
@@ -14,6 +16,14 @@
        //var resultat = document.getElementById("resultatNode");
        //resultat.innerHTML = JSON.parse(salution); 
    }
+   this.requetePositionBouee = function()
+   {
+       connection.emit('envoyer-position');
+   };
+ function recevoirPositionBouee(positions)
+ {
+    positionBouee = positions;
+ }
 
    this.requeteDonneeBouee = function()
    {
@@ -24,10 +34,12 @@
    {
     console.log("les donnee serait recu" + donnee);
    }
-
+   this.retournerPosition = function()
+   {
+       return positionBouee;
+   }
 
    init();
    this.requeteDonneeBouee();
 
 }
-)();
