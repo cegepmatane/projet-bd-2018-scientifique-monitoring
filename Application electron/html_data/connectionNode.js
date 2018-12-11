aab54356e569d1206dfca105807960751e34cbd9
Sedrick <sedrickidd@hotmail.com>
2018-12-11 15:23:55 -0500

    Finalisation de l'affichage des marqueur sur la carte #10

--- Application electron/html_data/connectionNode.js
+++ Application electron/html_data/connectionNode.js
@@ -1,50 +1,42 @@
-this.ConnectionNode = function ()
-{
-   var connection;
-   var positionBouee;
+this.ConnectionNode = function () {
+    var connection;
+    var positionBouee;
 
-   function init()
-   {
-       connection = io.connect('http://vps202433.vps.ovh.ca:8080');
-       connection.on('salutation', saluer);
-       connection.on('donneeBouee', recevoirDonneeBouee);
-       connection.on('positionBouee', recevoirPositionBouee);
-   }
+    function init() {
+        connection = io.connect('http://vps202433.vps.ovh.ca:8080');
+        connection.on('salutation', saluer);
+        connection.on('donneeBouee', recevoirDonneeBouee);
+        connection.on('donnees-marqueurs-bouee', recevoirPositionBouee);
+    }
 
-   function saluer(salution)
-   {
-       //var resultat = document.getElementById("resultatNode");
-       //resultat.innerHTML = JSON.parse(salution); 
-   }
-   this.requetePositionBouee = function()
-   {
-       connection.emit('envoyer-position');
-   };
+    function saluer(salution) {
+        //var resultat = document.getElementById("resultatNode");
+        //resultat.innerHTML = JSON.parse(salution); 
+    }
+    this.requetePositionBouee = function () {
+        connection.emit('requete-donnees-marqueurs');
+    };
 
- function recevoirPositionBouee(positions)
- {
-     console.log("ca rentre dans la fonction recevoir position");
-    positionBouee = positions;
-    console.log("socket des position: " + positions);
-    console.log("les position sont recu: " +positionBouee);
-    afficherMarqueur(positionBouee);
- }
+    function recevoirPositionBouee(positions) {
+        console.log("ca rentre dans la fonction recevoir position");
+        positionBouee = JSON.parse(positions);
+        //console.log("socket des position: " + positions);
+        console.log("les position sont recu: " + positionBouee);
+        afficherMarqueur(positionBouee);
+    }
 
-   this.requeteDonneeBouee = function()
-   {
-    connection.emit('envoyer-donnee');
-   };
+    this.requeteDonneeBouee = function () {
+        connection.emit('envoyer-donnee');
+    };
 
-   function recevoirDonneeBouee(donnee)
-   {
-    console.log("les donnee serait recu" + donnee);
-   }
-   this.retournerPosition = function()
-   {
-       return positionBouee;
-   }
+    function recevoirDonneeBouee(donnee) {
+        console.log("les donnee serait recu" + donnee);
+    }
+    this.retournerPosition = function () {
+        return positionBouee;
+    }
 
-   init();
-   this.requeteDonneeBouee();
+    init();
+    this.requeteDonneeBouee();
 
 }
